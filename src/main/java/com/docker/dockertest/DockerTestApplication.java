62fde004aa94e2f0b406a15585f75b0631375058
rivumitter <rivumitter@gmail.com>
2020-05-17 12:59:59 +0530

    test

--- src/main/java/com/docker/dockertest/DockerTestApplication.java
+++ src/main/java/com/docker/dockertest/DockerTestApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerTestApplication {
 	
-	@GetMapping("/test")
+	@GetMapping("/")
 	public String getMsg() {
 		
 		return "Hello Arghya!!";
